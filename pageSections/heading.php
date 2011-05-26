d28a9c6d9223e6f13b9e1a0ab1053608d643f428
Caroline <carolinej.is@googlemail.com>
2011-05-26 22:28:48 +0100

    trying to add a folder

---
+++ pageSections/heading.php
@@ -0,0 +1,4 @@
+  <div id="header">
+    <div id="headerTitle">James-Handley Wedding</div>
+    <img alt="A photo of mark and caroline smiling" id="headerImage" src="images/mark_and_caroline.gif">
+  </div>
---
+++ pageSections/menu.php
@@ -0,0 +1,6 @@
+   <ul id="menu">
+      <li><a href="anything">Home</a></li>
+      <li><a href="/food.php">Menus</a></li>
+      <li><a href="anything">Venue Info</a></li>
+      <li><a href="anything">Gifts</a></li>
+    </ul>
