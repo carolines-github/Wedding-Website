b537bfcc3c1318ae0eb80f6cd2500b8f04017692
Caroline <carolinej.is@googlemail.com>
2011-05-26 22:24:08 +0100

    adding website files

---
+++ food.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/font.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <?php include("pageSections/heading.php"); ?>
+  <div id="central-section">
+    <?php include("pageSections/menu.php"); ?>
+    <div id="main">Grub what we'll be having</div>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/font.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <?php include("pageSections/heading.php"); ?>
+  <div id="central-section">
+    <?php include("pageSections/menu.php"); ?>
+    <div id="main">blah blah blah</div>
+  </div>
+</body>
+</html>
